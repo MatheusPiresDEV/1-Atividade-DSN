287c1d8aaf6ccc7f788980dcf2b87e634386da91
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:20:15 -0300

    API

--- AulaNode/index.js
+++ AulaNode/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 3001;
+const nome = "CarlinhJos";
+let list = []
 
-app.get('/', (req, res) => {
-    res.send('Mastodonte');
-});//responsavel pela resposta 
+app.use(express.json())
+
+app.get('/visualizar', (req, res) => {
+    res.send(list);
+}); // responsável pela resposta
+
+app.post('/cadastrar',(req,res)=>{
+    const {nome , idade} = req.body;
+    list.push({nome,idade})
+    res.send(`Usuario cadastrado ${nome,idade}`)
+})
 
 app.listen(port, () => {
-    console.log(`Exemplo de Mastodonte na porta ${port}`);
-});//inicia o servidor 
+    console.log(`Exemplo de Maricon na porta ${port}`);
+}); // inicia o servidor 
+
+
+// API Rest trabalha com JSON
+// API SOAP trabalha com arquivos XML
+
